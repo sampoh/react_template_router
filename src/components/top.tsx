fcbd7c6a2c7f7e4327928b39bbc9b5eaa6484e25
Sampoh <me@mecrazy.net>
2023-01-27 13:30:07 +0900

    Add comment on TOP page

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -4,7 +4,23 @@ const Top = () => {
     return (
         <Fragment>
             <h1>TOP page</h1>
-            <div>※ ビルドしたものはRewrite等で全てindex.htmlを参照するようにしないと動作しないので注意。</div>
+            <table className="warning-table">
+                <thead>
+                    <tr>
+                        <th colSpan={2}>ビルド時の注意</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                        <td>Apache</td>
+                        <td>Rewrite等で実ファイルが無いリクエストは全てindex.htmlを参照するようにしないと動作しないので注意。</td>
+                    </tr>
+                    <tr>
+                        <td>4D</td>
+                        <td>On Web Connection で常時 WEB SEND FILE("index.html") とする必要あり。</td>
+                    </tr>
+                </tbody>
+            </table>
         </Fragment>
     )
 }
